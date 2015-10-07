224d64a4d4dc9d3d6507e4624a3891ca17ce6823
janinewong <jaeninwong@gmail.com>
2015-10-07 00:10:50 -0700

    Update

---
+++ main.js
@@ -0,0 +1,14 @@
+// main.js
+var client = new ZeroClipboard( document.getElementById("palette-block") );
+
+client.on( "ready", function( readyEvent ) {
+  // alert( "ZeroClipboard SWF is ready!" );
+
+  client.on( "aftercopy", function( event ) {
+    // `this` === `client`
+    // `event.target` === the element that was clicked
+    event.target.style.display = "none";
+    alert("Copied text to clipboard: " + event.data["text/plain"] );
+    
+  } );
+} );
