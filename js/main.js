62755d9a18747a25be67c0bce17e4ee09d03a1d4
oceanzou123 <ozou@sfu.ca>
2015-10-26 20:11:06 -0700

    download the jquery library

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
     $('nav ul').slideToggle(500);
-    //  $(".menuArrow").removeClass("fa-chevron-down");
-    // $(".menuArrow").addClass("fa-chevron-up");
+     $(".menuArrow").removeClass("fa-chevron-down");
+     $(".menuArrow").addClass("fa-chevron-up");
   });//end slide toggle
   
   $(window).resize(function() {   
